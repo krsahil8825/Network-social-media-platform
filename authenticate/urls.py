7cbd53d706dfbda25b4df2b928ab128274155169
Kumar Sahil <krsahil8825@gmail.com>
2025-10-10 01:48:32 +0530

    update landing page and create authenticate app for auth handeling (html is not completed in auth)

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls, name="admin"),
-    path("", include("core.urls")),
-    path("auth/", include("authenticate.urls")),
-    path("u/", include("users.urls")),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
 ]
