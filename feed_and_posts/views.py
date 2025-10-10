d348f7754bc360643ee3eb337d0e3452224dcde9
Kumar Sahil <krsahil8825@gmail.com>
2025-10-11 02:51:50 +0530

    implement pagination and post display in feed; add card and pagination components, enhance styles

--- feed_and_posts/views.py
+++ feed_and_posts/views.py
@@ -1,4 +1,10 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from .models import Post, Comment
 
 def feed_index(request):
-    return render(request, 'feed_and_posts/index.html', {})
+    posts = Post.objects.all().order_by('-created_at')
+    paginator = Paginator(posts, 10)  # Show 10 posts per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'feed_and_posts/index.html', {'page_obj': page_obj})
