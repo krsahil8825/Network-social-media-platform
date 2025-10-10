850b78ba30ef4f6b712e6a4a471020c3b7da9073
Kumar Sahil <krsahil8825@gmail.com>
2025-10-10 22:35:32 +0530

    enhance admin interface and models; add Post and Comment models with admin views, improve user display and search functionality in admin

--- admincontrol/admin.py
+++ admincontrol/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from core.models import ContactMessage
+from feed_and_posts.models import Post, Comment
 from users.models import User
 
 
@@ -10,7 +11,39 @@ admin.site.site_title = "Network Admin"
 
 
 # users app models
-admin.site.register(User)
+class UserView(admin.ModelAdmin):
+    list_display = ("username", "email", "is_active", "is_staff", "date_joined")
+    search_fields = ("username", "email")
+    list_filter = ("is_active", "is_staff", "date_joined")
+
+
+admin.site.register(User, UserView)
+
 
 # core app models
-admin.site.register(ContactMessage)
+class ContactMessageView(admin.ModelAdmin):
+    list_display = ("__str__", "user__username", "user__email", "created_at")
+    search_fields = ("user__username", "subject", "message")
+    list_filter = ("created_at", "user__is_active")
+
+
+admin.site.register(ContactMessage, ContactMessageView)
+
+
+# feed_and_posts app models
+class PostView(admin.ModelAdmin):
+    list_display = ("__str__", "user", "created_at")
+    search_fields = ("user__username", "title", "content")
+    list_filter = ("created_at",)
+    filter_horizontal = ("likes",)
+
+
+admin.site.register(Post, PostView)
+
+
+class CommentView(admin.ModelAdmin):
+    list_display = ("__str__", "user", "post", "created_at")
+    search_fields = ("user__username", "post__title", "content")
+    list_filter = ("created_at",)
+
+admin.site.register(Comment, CommentView)
--- feed_and_posts/models.py
+++ feed_and_posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from users.models import User
+import uuid
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField(default=uuid.uuid4, unique=True, editable=False)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, blank=True, related_name="liked_posts")
+
+    def __str__(self):
+        return f"Post by {self.user.username} on {self.created_at} - {self.title:.20}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.title:.20} - {self.created_at}"
