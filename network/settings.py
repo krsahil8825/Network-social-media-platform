31e8947496900626e423517f45f83dc3ec87c3db
Kumar Sahil <krsahil8825@gmail.com>
2025-10-10 12:10:26 +0530

    add authentication templates and styles; implement login and registration forms with responsive design

--- network/settings.py
+++ network/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 LOGIN_URL = "auth/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "auth/login/"
